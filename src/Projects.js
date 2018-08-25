0b34faab585507eebc1025cb9b00cb863abfb563
Bailey <bailey.siber@tufts.edu>
2018-08-25 14:18:58 -0400

    adding slack vid

--- src/Projects.js
+++ src/Projects.js
@@ -10,7 +10,6 @@ class Projects extends Component {
         <div className="Home-section">
           <hr className="Line"/>
           <p></p>
-          <p className="Desc">videos to come!</p>
           <p></p>
           <p></p>
           <a href="https://github.com/BaileySiber/HeadSmart-Frontend">
@@ -23,6 +22,12 @@ class Projects extends Component {
           <p className="Desc">
             Slack Bot: Sassy
           </p>
+          <p></p>
+
+
+<iframe title="slackbot" width="560" height="315" src="https://www.youtube.com/embed/acwEWRaiVmE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+
           </a>
           <p></p>
           <a href="https://github.com/BaileySiber/CouldntShareLess">
