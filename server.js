0c006a2b90eb21698d6b0003715f143153767344
Bailey <bailey.siber@tufts.edu>
2018-11-23 19:02:29 -0800

    updating server

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const Message = mongoose.model('Message', {
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json())
 
-var http = require("http");
-setInterval(function() {
-    http.get("https://bailey-site.herokuapp.com/");
-}, 1500000);
+// var http = require("http");
+// setInterval(function() {
+//     http.get("https://bailey-site.herokuapp.com/");
+// }, 1500000);
 
 app.get('/api/test', function(req, res) {
   res.send('success');
@@ -31,7 +31,7 @@ app.post('/api/contact', function (req, res) {
     .save()
     .then((result) => res.json(result))
     .catch((err) => {
-      console.log('didn not work!!')
+      console.log('did not work!!')
       res.status(500).end(err.message)
     })
 });
