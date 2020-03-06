a7aafe7ea7f71dd420ea91f8553427d9a98c8153
Bailey <bailey.siber@tufts.edu>
2020-03-05 22:20:09 -0500

    trying to add serverless

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.listen(process.env.PORT || 3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
+module.exports = app;
 module.exports.handler = serverless(app);
 
 //server.js
