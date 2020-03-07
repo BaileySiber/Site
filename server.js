044a80134796b3e1c135ddae727917ab056636bc
Bailey <bailey.siber@tufts.edu>
2020-03-07 12:43:01 -0500

    trying to add redirect

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const serverless = require('serverless-http');
 const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
@@ -41,7 +40,5 @@ app.listen(process.env.PORT || 3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
-module.exports = app;
-module.exports.handler = serverless(app);
 
 //server.js
