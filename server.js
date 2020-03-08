02ed81f27d073ce94a5398836e9fbb0869cb5564
Bailey <bailey.siber@tufts.edu>
2020-03-08 19:42:28 -0400

    CORS

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://bailey-site.herokuapp.com/");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
