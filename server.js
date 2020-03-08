fd61ea769e064456d0c6d5c95c552676d645973f
Bailey <bailey.siber@tufts.edu>
2020-03-08 00:03:36 -0500

    CORS bs

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://www.baileysiber.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Origin", "https://www.baileysiber.com");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
