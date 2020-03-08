e2e61a97afb751a865c02ebac5c4a29accd6f0cd
Bailey <bailey.siber@tufts.edu>
2020-03-07 23:57:01 -0500

    cant forget dm wwwwwws

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://baileysiber.com");
+  res.header("Access-Control-Allow-Origin", "https://www.baileysiber.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
