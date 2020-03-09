3e89f1e3b53c8c7f3f1958a6e4eed87d53ed66f9
Bailey <bailey.siber@tufts.edu>
2020-03-08 20:50:25 -0400

    cors agaaaain

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(function(req, res, next) {
 res.header('Access-Control-Allow-Origin: *');
 res.header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
 res.header('Access-Control-Request-Methods: GET, POST, PUT, OPTIONS')
-res.header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+res.header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
 next();
 });
 
