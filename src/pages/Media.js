04d8cef1892d806c906cb880bf8e5af7e5929848
Bailey <bailey.siber@tufts.edu>
2020-03-24 21:58:16 -0400

    adding dom-resizing ability plus some padding

--- src/pages/Media.js
+++ src/pages/Media.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
+import ReactResizeDetector from 'react-resize-detector';
 import '../css/Media.css';
 
 
 class Media extends Component {
+  constructor() {
+    super()
+    this.state = {
+      width: null,
+      height: null
+    }
+  }
+
+  onResize = (width, height) => {
+    console.log ("width is " + width + "height is " + height)
+    this.setState({width: width, height:height})
+  }
+
 
   render() {
     return (
       <div>
         <div className="Home-section">
 
+          {this.state.width > 500 ?
+
+          <div>
           <div className="mediaBox">
             <p className="item"><strong>Op-Ed</strong></p>
             <p className="item"> <a className="link" href="https://medium.com/@baileysiber37/will-massachusetts-step-up-for-survivors-681c116ae591">Medium: Will Massachusetts Step Up for Survivors? </a></p>
@@ -30,9 +48,40 @@ class Media extends Component {
             <p className="item"><a className="link" href="https://www.bostonglobe.com/metro/2019/02/22/college-students-gather-saturday-cambridge-for-summit-sexual-violence/YPK10MvgAPqv2cFdS1cyNP/story.html?s_campaign=bostonglobe%3Asocialflow%3Afacebook&fbclid=IwAR1p3jqLr1eE0gtKamzVgjvz7vwgHEoeddedN0H_4fs03UJf-fuVEJyXjXM">Boston Globe: Every Voice Summit</a></p>
             <p className="item"><a className="link" href="https://www.wbur.org/edify/2019/02/23/harvard-university-sexual-violence-summit?fbclid=IwAR2NNWvX18AokuXKq5f7L-9iNjx9HqU6DXVCMUPdWFUBUjEJ9BhC2RaxacE">WBUR: Every Voice Summit</a></p>
           </div>
+          </div>
 
-          <p></p>
+          :
+
+          <div>
+          <div>
+            <p className="item"><strong>Op-Ed</strong></p>
+            <p className="item"> <a className="link" href="https://medium.com/@baileysiber37/will-massachusetts-step-up-for-survivors-681c116ae591">Medium: Will Massachusetts Step Up for Survivors? </a></p>
         </div>
+
+          <div className="mediaBox">
+            <p className="item"><strong>Tufts Action For Sexual Assault Prevention (ASAP)</strong></p>
+            <p className="item"><a className="link" href="https://tuftsdaily.com/news/2018/04/11/happens-projects-voices-of-sexual-assault-survivors/">Tufts Daily: It Happens Here</a></p>
+            <p className="item"><a className="link" href="https://tuftsdaily.com/news/2017/09/21/oeo-monitor-devos-public-notice-process-sexual-misconduct-policy/">Tufts Daily: Betsy DeVos Proposed Policy</a></p>
+            <p className="item"><a className="link" href="https://tuftsdaily.com/features/2017/11/17/sexual-assault-resources-campus/">Tufts Daily: #MeToo Movement</a></p>
+            <p className="item"><a className="link" href="https://tuftsdaily.com/news/2018/04/11/students-gather-support-of-two-sexual-assault-prevention-bills/">Tufts Daily: State House Rally</a></p>
+          </div>
+
+          <div className="mediaBox">
+            <p className="item"><strong>The Every Voice Coalition</strong></p>
+            <p className="item"><a className="link" href="https://www.bostonglobe.com/metro/2018/08/09/prospects-murky-for-bill-address-campus-sexual-assault/H0JNakgfOioQceBuegBnOO/story.html">Boston Globe: Proposed Bills</a></p>
+            <p className="item"><a className="link" href="https://www.bostonglobe.com/metro/2018/04/10/college-students-rally-for-tougher-policies-prevent-campus-sexual-assaults/Cy70GFzss6QgYQ5RxtmnTM/story.html">Boston Globe: State House Rally</a></p>
+            <p className="item"><a className="link" href="https://www.bostonglobe.com/ideas/2019/03/01/colleges-won-protect-women-need-laws-that-will/nWP2a2a4ohCLTAspRWsvAO/story.html?fbclid=IwAR3ohQVsY_laa9JCEzKs2e5rnrUrE7U74C8aCkTHg_iE6sc7MlNVk8hw0WI">Boston Globe: Every Voice Summit</a></p>
+            <p className="item"><a className="link" href="https://www.bostonglobe.com/metro/2019/02/22/college-students-gather-saturday-cambridge-for-summit-sexual-violence/YPK10MvgAPqv2cFdS1cyNP/story.html?s_campaign=bostonglobe%3Asocialflow%3Afacebook&fbclid=IwAR1p3jqLr1eE0gtKamzVgjvz7vwgHEoeddedN0H_4fs03UJf-fuVEJyXjXM">Boston Globe: Every Voice Summit</a></p>
+            <p className="item"><a className="link" href="https://www.wbur.org/edify/2019/02/23/harvard-university-sexual-violence-summit?fbclid=IwAR2NNWvX18AokuXKq5f7L-9iNjx9HqU6DXVCMUPdWFUBUjEJ9BhC2RaxacE">WBUR: Every Voice Summit</a></p>
+          </div>
+          </div>
+
+        }
+
+        </div>
+
+        <ReactResizeDetector handleWidth handleHeight onResize={this.onResize} />
+
       </div>
     )}
   }
