3d31254e1f633978aeb9d25778e911492741245b
Bailey <bailey.siber@tufts.edu>
2020-04-25 00:56:41 -0400

    updated favicon and removed svg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import '../css/Home.css';
 import sun from '../images/sun.png'
 import building from '../images/buildings.png'
-import svg from '../images/drawing.svg'
-
 
 class Home extends Component {
   constructor() {
