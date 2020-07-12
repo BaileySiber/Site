6a13fbf11b722134c1177b3b435ec6cb2c16897c
Bailey <bailey.siber@tufts.edu>
2020-07-11 23:52:33 -0400

    gotta put spans around emojis for CI to be happy

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -14,7 +14,7 @@ class Info extends Component {
 
         <div className="infoChunk">
           <p>
-            hi! 👋🏼 i’m bailey, a project analyst/engineer/innovator that cares a whole lot about place based initiatives, using technology for social impact, & addressing the structural inequities that disproportionately impact marginalized communities.
+            hi! <span role="img" aria-label="wave">👋🏼 </span>i’m bailey, a project analyst/engineer/innovator that cares a whole lot about place based initiatives, using technology for social impact, & addressing the structural inequities that disproportionately impact marginalized communities.
           </p>
         </div>
 
@@ -26,13 +26,13 @@ class Info extends Component {
 
         <div className="infoChunk">
           <p>
-            in 2018 i graduated from tufts university 🐘 where i studied clinical psychology, community health, & a bit of computer science. i learned some more computer science post grad in a software engineering & entrepreneurship fellowship. while at tufts, I advocated for <a href="https://tuftsdaily.com/features/2017/11/17/sexual-assault-resources-campus/">better sexual violence prevention practices</a> & <a href="https://tuftsdaily.com/news/2018/04/11/happens-projects-voices-of-sexual-assault-survivors/">support for survivors</a> as president of a student led org - <a href="https://www.instagram.com/asapattufts/">asap</a>. I also <a href="https://medium.com/@baileysiber37/will-massachusetts-step-up-for-survivors-681c116ae591">championed improved legislation at the state level </a> while helping to lead at <a href="https://www.everyvoicecoalition.org/">the every voice coalition</a>.
+            in 2018 i graduated from tufts university <span role="img" aria-label="elephant"> 🐘 </span> where i studied clinical psychology, community health, & a bit of computer science. i learned some more computer science post grad in a software engineering & entrepreneurship fellowship. while at tufts, I advocated for <a href="https://tuftsdaily.com/features/2017/11/17/sexual-assault-resources-campus/">better sexual violence prevention practices</a> & <a href="https://tuftsdaily.com/news/2018/04/11/happens-projects-voices-of-sexual-assault-survivors/">support for survivors</a> as president of a student led org - <a href="https://www.instagram.com/asapattufts/">asap</a>. I also <a href="https://medium.com/@baileysiber37/will-massachusetts-step-up-for-survivors-681c116ae591">championed improved legislation at the state level </a> while helping to lead at <a href="https://www.everyvoicecoalition.org/">the every voice coalition</a>.
           </p>
         </div>
 
         <div className="infoChunk">
           <p>
-            outside of all this, I have a healthy obsession with plants 🌱& you'll likely find me hiking in the white mountains, singing, relearning arabic for the tenth time, or generally exploring.
+            outside of all this, I have a healthy obsession with plants <span role="img" aria-label="plant"> 🌱 </span>& you'll likely find me hiking in the white mountains, singing, relearning arabic for the tenth time, or generally exploring.
           </p>
         </div>
 
