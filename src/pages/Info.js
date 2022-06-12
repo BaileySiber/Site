46c2077b73fbefa11215cbdd7cdf3f74a23426bd
Bailey Siber <36415889+BaileySiber@users.noreply.github.com>
2022-06-12 13:30:28 -0400

    Update Info.js

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -14,14 +14,14 @@ class Info extends Component {
 
         <div className="infoChunk">
           <p>
-            i’m bailey, an arab american technologist focused on addressing spatial and economic inequities in cities through housing, third spaces, and workforce development initiatives.
+            i’m bailey, an arab american technologist focused on addressing racial inequities in cities through innovative economic development and placemaking initiatives.
           </p>
         </div>
 
 
         <div className="infoChunk">
           <p>
-            right now, i'm pursuing a masters in public policy as a rappaport fellow at the <a href="https://www.hks.harvard.edu/"> harvard kennedy school</a>, focusing on social and urban policy. i'm also leading ops and partnerships at <a href="https://www.thegcodehouse.com/">g&#123;code&#125;</a>, a non profit that empowers young women and non-binary people of color to gain economic mobility by providing housing, community, and technical training. i’m excited to be an alum of the <a href="https://www.tbf.org/what-we-do/strategic-focus-areas/nonprofit-effectiveness/women-of-color-leadership-circle">anna faith jones & frieda garcia women of color leadership circle</a>.
+            right now, i'm pursuing a masters in public policy as a rappaport fellow at the <a href="https://www.hks.harvard.edu/"> harvard kennedy school</a>, focusing on social and urban policy. i also serve as an advisor (and was a founding team member) at <a href="https://www.thegcodehouse.com/">g&#123;code&#125;</a>, a non profit that empowers Black, brown, and Indigenous women and non-binary folks to gain economic mobility by providing housing, community, and technical training. i’m also a happy alum of the <a href="https://www.tbf.org/what-we-do/strategic-focus-areas/nonprofit-effectiveness/women-of-color-leadership-circle">anna faith jones & frieda garcia women of color leadership circle</a>.
           </p>
         </div>
 
@@ -33,7 +33,7 @@ class Info extends Component {
 
         <div className="infoChunk">
           <p>
-            please feel free to reach out to learn more about g&#123;code&#125;, brainstorm new ideas, share great music, or anything else!
+            please feel free to reach out to learn more about g&#123;code&#125;, talk civic innovation, brainstorm new ideas, share great music, or anything else!
           </p>
         </div>
 
