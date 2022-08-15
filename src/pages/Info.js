5838707bcff7ddca6e484898b19b0e9a16d126b5
Bailey Siber <36415889+BaileySiber@users.noreply.github.com>
2022-08-15 15:03:17 -0400

    Update Info.js

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -14,14 +14,14 @@ class Info extends Component {
 
         <div className="infoChunk">
           <p>
-            i’m bailey, an arab american technologist focused on addressing racial inequities in cities through innovative economic development and placemaking initiatives.
+            i’m bailey, an arab american technologist focused on addressing spatial, economic, and racial inequities in cities through innovative community and economic development as well as placemaking initiatives.
           </p>
         </div>
 
 
         <div className="infoChunk">
           <p>
-            right now, i'm pursuing a masters in public policy as a rappaport fellow at the <a href="https://www.hks.harvard.edu/"> harvard kennedy school</a>, focusing on social and urban policy. i also serve as an advisor (and was a founding team member) at <a href="https://www.thegcodehouse.com/">g&#123;code&#125;</a>, a non profit that empowers Black, brown, and Indigenous women and non-binary folks to gain economic mobility by providing technical training and wraparound supports. i’m also a happy alum of the <a href="https://www.tbf.org/what-we-do/strategic-focus-areas/nonprofit-effectiveness/women-of-color-leadership-circle">anna faith jones & frieda garcia women of color leadership circle</a>.
+            right now, i'm pursuing a masters in public policy as a <a href="https://rappaportfoundation.org/initiatives/publicpolicy/> rappaport fellow </a> at the <a href="https://www.hks.harvard.edu/"> harvard kennedy school</a>, focusing on social and urban policy. i also serve as an advisor at <a href="https://www.thegcodehouse.com/">g&#123;code&#125;</a>, a non profit that empowers Black, brown, and Indigenous women and non-binary folks to gain economic mobility by providing technical training and wraparound supports. i’m also a happy alum of the <a href="https://www.tbf.org/what-we-do/strategic-focus-areas/nonprofit-effectiveness/women-of-color-leadership-circle">the boston foundation's women of color leadership circle</a> and the <a href="https://www.boston.gov/departments/new-urban-mechanics"> MONUM </a> civic innovation fellowship.
           </p>
         </div>
 
@@ -33,7 +33,7 @@ class Info extends Component {
 
         <div className="infoChunk">
           <p>
-            please feel free to reach out to learn more about g&#123;code&#125;, talk civic innovation, brainstorm new ideas, share great music, or anything else!
+            please feel free to reach out to learn more about g&#123;code&#125;, brainstorm new ideas, share great music, or anything else!
           </p>
         </div>
 
