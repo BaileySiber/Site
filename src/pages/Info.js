f52206d3b78b56331ac52af8ca9db136ea731097
Bailey Siber <36415889+BaileySiber@users.noreply.github.com>
2024-11-20 14:39:25 -0600

    Update Info.js
    updates to my life

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -14,26 +14,26 @@ class Info extends Component {
 
         <div className="infoChunk">
           <p>
-            hi! i’m bailey, a technologist and researcher focused on economic mobility and empowerment, racial justice, and state and local governments. 
+            hi, i’m bailey! 
           </p>
         </div>
 
 
         <div className="infoChunk">
           <p>
-            right now, i'm serving as a two year <a href="https://www.cityleadership.harvard.edu/city-hall-fellowships"> bloomberg harvard city leadership fellow</a>, supporting oklahoma city as they launch their civic innovation team. i recently completed my masters in public policy as a <a href="https://rappaportfoundation.org/initiatives/publicpolicy/"> rappaport fellow</a> at the <a href="https://www.hks.harvard.edu/"> harvard kennedy school</a>, concentrating in social and urban policy. additionally, i serve as an advisor at <a href="https://www.thegcodehouse.com/">g&#123;code&#125;</a>, and am a happy alum of the boston foundation's <a href="https://www.tbf.org/what-we-do/strategic-focus-areas/nonprofit-effectiveness/women-of-color-leadership-circle">women of color leadership circle </a> and the city of boston's <a href="https://www.boston.gov/departments/new-urban-mechanics"> civic innovation fellowship</a>.
+            i am currently working in the city of oklahoma city's <a href="https://www.okc.gov/government/office-of-innovation"> office of innovation</a> as a <a href="https://www.cityleadership.harvard.edu/city-hall-fellowships"> bloomberg harvard city hall fellow</a>, working on initiatives ranging from data governance to homelessness. i recently completed my masters in public policy as a <a href="https://rappaportfoundation.org/initiatives/publicpolicy/"> rappaport fellow</a> at the <a href="https://www.hks.harvard.edu/"> harvard kennedy school</a>, concentrating in social and urban policy. i was a founding team member at <a href="https://www.thegcodehouse.com/">g&#123;code&#125;</a> and am a happy alum of the boston foundation's <a href="https://www.tbf.org/what-we-do/strategic-focus-areas/nonprofit-effectiveness/women-of-color-leadership-circle">women of color leadership circle </a> and the city of boston's <a href="https://www.boston.gov/departments/new-urban-mechanics"> civic innovation fellowship</a>.
           </p>
         </div>
 
         <div className="infoChunk">
           <p>
-            in my spare time, you'll find me exploring public libraries and parks, sewing, or watching vocal coach reactions on youtube.
+            in my spare time, you'll find me exploring public libraries and parks, sewing, or making pottery.
           </p>
         </div>
 
         <div className="infoChunk">
           <p>
-            please feel free to reach out to learn more about g&#123;code&#125;, brainstorm new ideas, share great music, or anything else!
+            please feel free to reach out to brainstorm new ideas, share great music, or anything else!
           </p>
         </div>
 
