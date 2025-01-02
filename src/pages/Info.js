281bb64ef5382171c687e5955d8c3a3b2e42741b
Bailey Siber <36415889+BaileySiber@users.noreply.github.com>
2025-01-01 21:26:31 -0600

    Update Info.js
    job updates / life updates

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -21,19 +21,19 @@ class Info extends Component {
 
         <div className="infoChunk">
           <p>
-            i am currently working in the city of oklahoma city's <a href="https://www.okc.gov/government/office-of-innovation"> office of innovation</a> as a <a href="https://www.cityleadership.harvard.edu/city-hall-fellowships"> bloomberg harvard city hall fellow</a>, working on initiatives ranging from data governance to homelessness. i recently completed my masters in public policy as a <a href="https://rappaportfoundation.org/initiatives/publicpolicy/"> rappaport fellow</a> at the <a href="https://www.hks.harvard.edu/"> harvard kennedy school</a>, concentrating in social and urban policy. i was a founding team member at <a href="https://www.thegcodehouse.com/">g&#123;code&#125;</a> and am a happy alum of the boston foundation's <a href="https://www.tbf.org/what-we-do/strategic-focus-areas/nonprofit-effectiveness/women-of-color-leadership-circle">women of color leadership circle </a> and the city of boston's <a href="https://www.boston.gov/departments/new-urban-mechanics"> civic innovation fellowship</a>.
+            i am currently working as the deputy chief of staff to the <a href="https://www.okc.gov/government/mayor"> mayor of oklahoma city</a>. before that, i was working in oklahoma city's <a href="https://www.okc.gov/government/office-of-innovation"> office of innovation</a> as a <a href="https://www.cityleadership.harvard.edu/city-hall-fellowships"> bloomberg harvard city hall fellow</a>, working on initiatives ranging from data governance to homelessness. prior to okc, i completed my masters in public policy as a <a href="https://rappaportfoundation.org/initiatives/publicpolicy/"> rappaport fellow</a> at the <a href="https://www.hks.harvard.edu/"> harvard kennedy school</a>, worked with the city of boston's <a href="https://www.boston.gov/departments/new-urban-mechanics"> civic innovation team</a>, helped launch the nonprofit <a href="https://www.thegcodehouse.com/">g&#123;code&#125;</a>, and held various technologist roles at a few startups. 
           </p>
         </div>
 
         <div className="infoChunk">
           <p>
-            in my spare time, you'll find me exploring public libraries and parks, sewing, or making pottery.
+            in my spare time, you'll find me making pottery, enjoying live music, or exploring okc with my pup summit.
           </p>
         </div>
 
         <div className="infoChunk">
           <p>
-            please feel free to reach out to brainstorm new ideas, share great music, or anything else!
+            please feel free to reach out to chat local gov, share great music, or anything else!
           </p>
         </div>
 
